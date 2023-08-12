0c550ff70d80a2c276df8462d2b71bffb614e245
Usmonov Azizbek <usmonovazizbek045@gmail.com>
2023-08-12 12:05:18 +0500

    i18n bug fix

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,14 +1,9 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import Backend from "i18next-http-backend"
-import LanguageDetector from "i18next-browser-languagedetector"
 
-i18next.use(Backend).use(initReactI18next).use(LanguageDetector).init({
+i18next.use(Backend).use(initReactI18next).init({
     fallbackLng: ["uz", "ru", "en"],
-    debug: true,
-    interpolation: {
-        escapeValue: false
-    }
 })
 
 export default i18next
